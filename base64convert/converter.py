68a397e365aa7d5dc31dd0c5ee56e6874328bb29
LaraArgento <lara.agnt@gmail.com>
2020-07-10 16:05:45 -0300

    python PDF to base64 converter

---
+++ base64convert/converter.py
@@ -0,0 +1,13 @@
+import base64
+
+
+# Lara Argento
+# Convert a PDF file to base64 string. 
+with open("arquivoPDF.pdf", "rb") as pdf_file:
+    encoded_bytes = base64.b64encode(pdf_file.read())
+    encoded_string = encoded_bytes.decode('utf-8')
+    #print(encoded_string)
+
+f = open("base64string.txt", "w")
+f.write(encoded_string)
+f.close()
